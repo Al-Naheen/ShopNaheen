aaed1a4c0b5cceed6116e57d68f2cb47bdd640fe
Al Naheen <alnaheen543@gmail.com>
2021-09-09 11:29:24 +0600

    Implementing Redux for State Management

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ShopNaheen</Col>
+          <Col className='text-center py-3'>Copyright &copy; Shop Naheen</Col>
         </Row>
       </Container>
     </footer>

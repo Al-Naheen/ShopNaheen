8d094292a15adb39f092c58ec547308bede4f20b
Al Naheen <alnaheen543@gmail.com>
2021-09-11 00:00:02 +0600

    Checkout part 2 - Order Screen & Paypal-Venmo-Credit-Debit payments

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; Shop Naheen</Col>
         </Row>
       </Container>
     </footer>
